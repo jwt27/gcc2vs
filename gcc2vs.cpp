068ff14a27ea04a4acbe87cccbd95cda23a104dc
J.W. Jagersma <jwjagersma@gmail.com>
2017-03-10 15:36:52 +0100

    add license

--- gcc2vs.cpp
+++ gcc2vs.cpp
@@ -1,3 +1,20 @@
+/****************************** gcc2vs *******************************
+Copyright (C) 2017  J.W. Jagersma
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
 #include <iostream>
 #include <string>
 #include <regex>
@@ -40,8 +57,7 @@ int main()
     convert_slash(path);
     std::size_t pos = 0;
     while ((pos = path.find('\n', pos)) != std::string::npos)
-        path.erase(pos, 1);
-        
+        path.erase(pos, 1);        
     if (path.back() != '\\') path += '\\';
 
     int error = 0;
